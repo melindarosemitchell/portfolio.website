745f8211fa37015f3fc9bc90e326f3eeafae0237
melindarosemitchell <47802991+melindarosemitchell@users.noreply.github.com>
2021-09-08 11:43:32 +1000

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,16 @@
+//comments use this
+/*multiline comments use this*/
+
+alert('If you are reading this, you are adorable');
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+
+function openMenu() {
+    var x = document.getElementById("portfolioTopnav");
+    if (x.className === "topnav") {
+    x.className += "responsive";
+    } 
+    else {
+    x.className = "topnav";
+    }
+}
